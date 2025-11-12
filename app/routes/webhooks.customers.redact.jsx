fbc7099e5a56647e65029c8aeb47bfa9c0551f58
Mark <learningexpress277@gmail.com>
2025-11-12 16:31:05 -0500

    Make webhook responses JSON 200 OK for compliance

--- app/routes/webhooks.customers.redact.jsx
+++ app/routes/webhooks.customers.redact.jsx
@@ -3,5 +3,9 @@ import { authenticate } from "../shopify.server";
 export const action = async ({ request }) => {
   await authenticate.webhook(request);
   console.log("Received customers/redact webhook");
-  return new Response("OK", { status: 200 });
+
+  return new Response(JSON.stringify({ success: true }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 };
